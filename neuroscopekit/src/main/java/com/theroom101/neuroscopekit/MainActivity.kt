f84569e30080016641af8320e283b766b5d692ee
TheRoom101 <deeman30rus@gmail.com>
2020-09-08 16:08:41 +0300

    base modules

---
+++ neuroscopekit/src/main/java/com/theroom101/neuroscopekit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.theroom101.neuroscopekit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
